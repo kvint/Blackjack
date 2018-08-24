765237c01a5f9173691aac6544cb85db7943373c
Alexander Slavschik <alexander.slavschik@gismart.com>
2018-08-24 16:13:29 +0300

    gitignore updated

---
+++ Blackjack2DPrototype/Blackjack2DPrototype/GameActionDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  GameActionDelegate.swift
+//  Blackjack2DPrototype
+//
+//  Created by Александр Славщик on 17.03.2018.
+//  Copyright © 2018 Александр Славщик. All rights reserved.
+//
+
+import Foundation
